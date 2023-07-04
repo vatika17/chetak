a7d36bd459a6a05f24e7b986af631fb6c02b257f
Vatika Agrawal <100186186+vatika17@users.noreply.github.com>
2023-07-04 17:39:58 +0530

    Update earthquake.py

--- earthquake.py
+++ earthquake.py
@@ -10,7 +10,6 @@ from sklearn.metrics import mean_squared_error
 data = pd.read_csv('earthquake.csv')
 
 # Step 2: Feature extraction
-# Assuming you have features 'magnitude', 'depth', and 'latitude'
 features = ['Magnitude', 'Depth', 'Latitude']
 target = 'Longitude'  # Assuming 'longitude' is the target variable
 
@@ -20,7 +19,7 @@ y = data[target]
 # Step 3: Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Step 4: Build and train the SVR model
+# Step 4: Build and train the model
 model = RandomForestRegressor()
 model.fit(X_train, y_train)
 
